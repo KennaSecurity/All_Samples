aaff77304fc0bd53b7d708f0b920a271a15b1842
jaredkalmus <73199391+jaredkalmus@users.noreply.github.com>
2021-09-01 15:42:07 -0500

    Update add_users_MRPU.rb
    Adds phone number field and removes need to pass column name arguments

--- add_users/add_users_MRPU.rb
+++ add_users/add_users_MRPU.rb
@@ -7,10 +7,10 @@ require 'csv'
 #These are the arguments we are expecting to get - header file can be send as third parameter if not included as row 1 in csv
 @token = ARGV[0]
 @csv_file = ARGV[1]
-@fname_col = ARGV[2]
-@lname_col = ARGV[3]
-@roles_col = ARGV[4]
-@email_col = ARGV[5]
+#@fname_col = ARGV[2]
+#@lname_col = ARGV[3]
+#@roles_col = ARGV[4]
+#@email_col = ARGV[5]
 
 #Variables we'll need later
 @post_url = 'https://api.kennasecurity.com/users'
@@ -23,32 +23,31 @@ puts "Found #{num_lines} lines."
 CSV.foreach(@csv_file, :headers => true){|row|
   # "Reading line #{$.}... "
   current_line = $.
-  fname = nil
-  lname = nil
-  email = nil
-  roles = nil
 
-  fname = row[@fname_col]
-  lname = row[@lname_col]
-  email = row[@email_col]
-  roles = row[@roles_col].split(",").collect(&:strip)
+  email = row[0]
+  fname = row[1]
+  lname = row[2]
+  phone = row[3]
+  roles = row[4].split(",").collect(&:strip)
 
   #puts roles [remove comment for troubleshooting]
 
+  #build json payload
   json_data = JSON.generate({
     "user" =>
     {
       "firstname"=>fname,
       "lastname"=>lname,
       "email"=>email,
+      "phone"=>phone,
       "roles"=>roles
         }
     }
   )
 
-    puts json_data
+    #puts json_data [remove comment for troubleshooting]
 
-          #puts json_data
+          #builds api request and sends it to the platform
           begin
             query_post_return = RestClient::Request.execute(
               :method => :post,
@@ -63,3 +62,5 @@ CSV.foreach(@csv_file, :headers => true){|row|
 
           end
 }
+
+puts "Complete!"

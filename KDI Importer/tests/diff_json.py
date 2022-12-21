6ea81c858f065861d01876da3eed675e0ad58ee9
Rick Ehrhart <77027682+rick-kenna@users.noreply.github.com>
2022-12-21 07:55:50 -0800

    Fixed the ordered(list) TypeError.  Modified some output. Added an ordered file JSON dump if there was a difference. (#174)

--- KDI Importer/tests/diff_json.py
+++ KDI Importer/tests/diff_json.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 
@@ -5,20 +6,43 @@ def print_help(prog_name):
     print(f"{prog_name} <json_file1> <json_file2>")
     sys.exit(1)
 
+# Dumps ordered json data into a file.
+def dump_ordered_json(ordered_json_data, file_path):
+    (file_name, suffix) = os.path.splitext(file_path)
+    ordered_json_file = f"{file_name}_ordered{suffix}" 
+
+    with open(ordered_json_file, 'w') as ojf_fp:
+        json.dump(ordered_json_data, ojf_fp, sort_keys=True, indent=2)
+
+    return ordered_json_file
+
+# Process None for an item or tuple.
+def none_sorter(item):
+    if item is None:
+        return ""
+    if isinstance(item, tuple) and item[1] is None:
+        return (item[0], "")
+    return item
+
 # Recursively sorts any lists it finds (and convert dictionaries to 
 # lists of (key, value) pairs so that they're orderable):
 # From: https://stackoverflow.com/questions/25851183/how-to-compare-two-json-objects-with-the-same-elements-in-a-different-order-equa
 def ordered(obj):
+    # This check for None allowed the `sorted((ordered(x) for x in obj), key=none_sorter)`
+    # to not get a TypeError.  I think the statement ends the recursion.
+    if obj is None:
+        return ""
+
     if isinstance(obj, dict):
-        return sorted((k, ordered(v)) for k, v in obj.items())
+        return sorted(((k, ordered(v)) for k, v in obj.items()), key=none_sorter)
     if isinstance(obj, list):
-        return sorted(ordered(x) for x in obj)
+        return sorted((ordered(x) for x in obj), key=none_sorter)
     else:
         return obj
 
 if __name__ == "__main__": 
     if len(sys.argv) != 3:
-        print(f"Argument length should be 3 ({len(sys.argv)})")
+        print(f"{sys.argv[0]} should have 2 arguments.  It has {len(sys.argv)}")
         print_help(sys.argv[0])
 
     # Get the file names and intialisze dictionaries.
@@ -61,10 +85,21 @@ if __name__ == "__main__":
         print("No JSON in {json_file2")
         sys.exit(1)
 
+    # Let's do a length compare for grins.
+    if len(json_data1) != len(json_data2):
+        print(f"Data length {len(json_data1)} from {json_file1} is not equal to data length {len(json_data2)} from {json_file2}")
+        print(f"Continuing with diff")
+
     ordered_json_data1 = ordered(json_data1)
+
     ordered_json_data2 = ordered(json_data2)
+    
+    # Now diff the JSON data.
     if ordered_json_data1 != ordered_json_data2: 
-        print(f"{json_file1} and {json_file2} have diferences.  Please manually check.")
+        ordered_json_file1 = dump_ordered_json(ordered_json_data1, json_file1)
+        ordered_json_file2 = dump_ordered_json(ordered_json_data2, json_file2)
+        print(f"{json_file1} and {json_file2} have diferences.")
+        print(f"Please manually check {ordered_json_file1} and {ordered_json_file2}")
         sys.exit(1)
 
     # Files match!
